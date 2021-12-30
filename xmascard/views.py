f7c0293c05b829e7263fcd58cf2e6e9facb42da7
Nicholas Gunther <nlgunther@gmail.com>
2021-12-29 16:51:51 -0800

    Working local xmascard with photos and static files.

--- xmascard/views.py
+++ xmascard/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'xmascard/post_list.html', {'posts': posts})
+    
+# def obituary(request):
+    # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # return render(request, 'xmascard/obituary.html', {'posts': posts})
